025de5c84c6770737db5b9e49ddf0b98c57bca45
DmitryFedoreev <fedoreev.doc@gmail.com>
2024-08-09 18:26:04 +0500

    fix bug

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ var romanToIntMap = map[string]int{
 var intToRomanMap = map[int]string{
 	1: "I", 2: "II", 3: "III", 4: "IV", 5: "V",
 	6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
+	20: "XX", 30: "XXX", 40: "XL", 50: "L",
+	60: "LX", 70: "LXX", 80: "LXXX", 90: "XC", 100: "C",
 }
 
 func toInt(roman string) (int, error) {
@@ -29,10 +31,15 @@ func toRoman(num int) (string, error) {
 	if num < 1 {
 		return "", errors.New("результат меньше единицы для римских чисел недопустим")
 	}
-	if val, ok := intToRomanMap[num]; ok {
-		return val, nil
+
+	var result strings.Builder
+	for _, value := range []int{100, 90, 50, 40, 10, 9, 5, 4, 1} {
+		for num >= value {
+			result.WriteString(intToRomanMap[value])
+			num -= value
+		}
 	}
-	return "", errors.New("число выходит за пределы поддерживаемых значений")
+	return result.String(), nil
 }
 
 func calculate(expression string) (string, error) {
